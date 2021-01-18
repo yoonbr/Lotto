aab0225e27c9a95cb8f81df826556ef4c9a42c49
yoonbr <yoon_br@naver.com>
2021-01-18 15:38:29 +0900

    20210118

---
+++ app/src/main/java/com/yoond/lotto/ConstellationActivity.kt
@@ -0,0 +1,11 @@
+package com.yoond.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation_acti_vity)
+    }
+}
